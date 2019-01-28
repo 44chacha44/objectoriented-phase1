c2f3b3e863f747e654f6fdd8b755b2d09a8f90dd
Nehomah Mora <nehomahm@gmail.com>
2019-01-28 09:00:04 -0700

    added activation token accessor and mutator, fixed errors

--- php/Classes/Foo.php
+++ php/Classes/Foo.php
@@ -37,7 +37,7 @@ class Author {
 	 * #return int value of author id
 	 */
 	public function getAuthorId(): Uuid {
-		return($this->authorId);
+		return ($this->authorId);
 	}
 
 	/**
@@ -46,14 +46,78 @@ class Author {
 	 * @param int $newAuthorId new value of author id
 	 * @throws UnexpectedValueException if $newAuthorId is not an integer
 	 */
-	public function setAuthorId( $newAuthorId) : {
-	//verify the author id is valid
-		$newAuthorId = filter_var($newAuthorId, FILTER_VALIDATE_INT);
-	if ($newAuthorId === false){
-		throw(new UnexpectedValueException("profile id is not a valid integer"));
+	public function setAuthorId($newAuthorId): void {
+		try {
+			$uuid = self::validateUuid($newAuthorId);
+		} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+			$exceptionType = get_class($exception);
+			throw(new $exceptionType($exception->getMessage(), 0, $exception));
 		}
-	//convert and store the author id
-		$this->authorId = intval($newAuthorId);
+		//convert and store the author id
+		$this->authorId = $uuid;
+	}
+
+	/**
+	 * accessor method for author email
+	 *
+	 * return author email
+	 */
+	public function getAuthorEmail(): string {
+		return ($this->authorEmail);
+	}
+
+	/**mutator method for author email
+	 *
+	 * @param string $newAuthorEmail new value of email
+	 * @throws \InvalidArgumentException if $newEmail is not a valid email or insecure
+	 * @throws  \RangeException if $newEmail is > 128 characters
+	 * @throws \TypeError if $newEmail is not a string
+	 * */
+	public function setAuthorEmail(string $newAuthorEmail): void {
+		//verify the email is secure
+		$newAuthorEmail = trim(@$newAuthorEmail);
+		$newAuthorEmail = filter_var($newAuthorEmail, FILTER_VALIDATE_EMAIL);
+		if(empty(@$newAuthorEmail) === true) {
+			throw(new \InvalidArgumentException("author email is empty or insecure"));
+		}
+		// verify the email will fit in the database
+		if(strlen(@$newAuthorEmail) > 128) {
+			throw(new \RangeException("author email is too large"));
+		}
+		// store the email
+		$this->authorEmail = $newAuthorEmail;
+	}
+
+	/**
+	 * Accessor method for author activation token
+	 *
+	 * return author activation token
+	 */
+	public function getAuthorActivationToken(): string {
+		return ($this->authorActivationToken);
+	}
+
+	/**
+	 * Mutator method for author activation token
+	 *
+	 * @param string $newAuthorActivationToken
+	 * @throws \InvalidArgumentException if the token is not a string or insecure
+	 * @throws \RangeException if the token is not exactly 32 characters
+	 * @throws \TypeError if the activation token is not a string
+	 */
+	public function setAuthorActivationToken(?string $newAuthorActivationToken): void {
+		if($newAuthorActivationToken === null) {
+			$this->newAuthorActivationTokent = null;
+			return;
+		}
+		$newAuthorActivationToken = strtolower(trim($newAuthorActivationToken));
+		if(ctype_xdigit($newAuthorActivationToken) === false) {
+			throw (new\RangeException("author activation is not valid"));
+		}
+		//make sure activation toke is only 32 characters
+		if(strlen($newAuthorActivationToken) !== 32) {
+			throw(new\RangeException("author activation token has to be 32"));
+		}
+		$this->authorActivationToken = $newAuthorActivationToken;
 	}
 }
-?>
