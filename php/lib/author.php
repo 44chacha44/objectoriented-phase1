f95fa8d536d97c3aaafdd2fadfc8a9bb2cfcae86
Nehomah Mora <nehomahm@gmail.com>
2019-01-29 16:13:10 -0700

    corrected syntax errors

--- php/lib/author.php
+++ php/lib/author.php
@@ -3,4 +3,15 @@
 // use the use keyword to specify the namespace you want to use
 // make sure to stay loyal to the constructor.
 //var_dump the object
-$newObject = new Author();
+
+namespace Nmora9\Author;
+
+require_once(dirname(__DIR__, 1) . "/Classes/Author.php");
+
+
+use Ramsey\Uuid\Uuid;
+
+$newAuthorId = newAuthorId("6b3eadae-c9c3-4f69-a89c-7501ccfa6d65", "hehehehehehehehehehehehehehehehe",
+"www.avatarwebsite.com", "nehomahm@gmail.com","nanananananananananananananananananananananananananananananananananananananananananananananananan",
+"Brilliant Writer");
+var_dump($newAuthorId);
